85392c33deaa0214309722ad2c7768f1a27c0cb7
salhotra <nishantsalhotra.ns@gmail.com>
2024-02-04 21:29:27 +0530

    Add todo comments

--- src/app/components/HomeContent.tsx
+++ src/app/components/HomeContent.tsx
@@ -10,6 +10,7 @@ export default function HomeContent() {
     <div className="flex flex-1 flex-col text-white">
       <div className="flex flex-col items-center justify-center h-[720px] relative">
         <div className="z-0 absolute top-0 right-0 bottom-0 left-0">
+          {/* TODO: Make sure this looks good on slow internet */}
           <Image
             priority
             src="/hero-1.jpg"
@@ -21,6 +22,7 @@ export default function HomeContent() {
           <div className="absolute bg-gradient-to-r from-neutral-950 to-neutral-900 opacity-75 top-0 bottom-0 left-0 right-0" />
         </div>
         <div className="flex flex-col items-center justify-center z-10 relative">
+          {/* TODO: Make it better for mobile */}
           <h1
             className={clsx(
               playfair.className,
