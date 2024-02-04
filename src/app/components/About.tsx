845f8891565ddcd3698e53439d584310b17136e6
salhotra <nishantsalhotra.ns@gmail.com>
2024-02-04 21:24:29 +0530

    Add about-us section

---
+++ src/app/components/About.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl mb-8 mt-4 px-8">About Us</h1>
+      <div>
+        We at SD Designs are a team of passionate designers who are dedicated to
+        creating beautiful spaces for our clients. We believe that the spaces we
+        inhabit should be a reflection of our personalities and our lifestyles.
+        We are committed to creating spaces that are not only beautiful but also
+        functional. We believe that good design should be accessible to everyone
+        and we strive to create spaces that are not only beautiful but also
+        affordable. We are committed to creating spaces that are not only
+        beautiful but also functional. We believe that good design should be
+        accessible to everyone and we strive to create spaces that are not only
+        beautiful but also affordable. We are committed to creating spaces that
+        are not only beautiful but also functional. We believe that good design
+        should be accessible to everyone and we strive to create spaces that are
+        not only beautiful but also affordable. We are committed to creating
+        spaces that are not only beautiful but also functional. We believe that
+        good design should be accessible to everyone and we strive to create
+        spaces that are not only beautiful but also affordable. We are committed
+        to creating spaces that are not only beautiful but also functional. We
+        believe that good design should be accessible to everyone and we strive
+        to create spaces that are not only beautiful but also affordable. We are
+        committed to creating spaces that are not only beautiful but also
+        functional. We believe that good design should be accessible to everyone
+        and we strive to create spaces that are not only beautiful but also
+        affordable. We are committed to creating spaces that are not only
+        beautiful but also functional. We believe that good design should be
+        accessible to everyone and we strive to create spaces that are not only
+        beautiful but also affordable. We are committed to creating spaces that
+        are not only beautiful but also functional. We believe that good design
+        should be accessible to everyone and we strive to create spaces that are
+        not only beautiful but also affordable.
+      </div>
+    </div>
+  );
+}
