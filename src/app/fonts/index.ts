f9e5c941aad79761f1ef3445bcbb83cc2570fee6
salhotra <nishantsalhotra.ns@gmail.com>
2024-02-04 21:17:52 +0530

    Add fonts

---
+++ src/app/fonts/index.ts
@@ -0,0 +1,5 @@
+import { Josefin_Sans, Playfair_Display, Roboto_Slab } from "next/font/google";
+
+export const playfair = Playfair_Display({ subsets: ["latin"] });
+export const josephinSans = Josefin_Sans({ subsets: ["latin"] });
+export const robotoSlab = Roboto_Slab({ subsets: ["latin"] });
