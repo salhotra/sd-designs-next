cad6ecaa69f63e1526b45acafa1426a4f33b6f15
salhotra <nishantsalhotra.ns@gmail.com>
2024-05-09 22:29:40 +0530

    Reduce overlay opacity

--- src/app/components/Overlay.tsx
+++ src/app/components/Overlay.tsx
@@ -4,7 +4,7 @@ export default function Overlay({ children }: { children?: React.ReactNode }) {
   return (
     <div className="z-0 absolute top-0 right-0 bottom-0 left-0">
       {children}
-      <div className="absolute bg-gradient-to-r from-neutral-950 to-neutral-900 opacity-70 top-0 bottom-0 left-0 right-0" />
+      <div className="absolute bg-gradient-to-r from-neutral-950 to-neutral-900 opacity-50 top-0 bottom-0 left-0 right-0" />
     </div>
   );
 }
