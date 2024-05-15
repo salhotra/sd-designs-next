bd84c40ad490b005bdb2e37cd587c88dcfd95046
salhotra <nishantsalhotra.ns@gmail.com>
2024-05-15 20:00:52 +0530

    Add og:title meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,8 @@
-import type { Metadata } from "next";
 import clsx from "clsx";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 import { montserrat } from "./fonts";
-import { CompanyName } from "./constants";
-
-const metadata: Metadata = {
-  title: CompanyName,
-  description: `${CompanyName} is an interior design company based in New Delhi.`,
-};
 
 export default function RootLayout({
   children,
@@ -24,7 +17,15 @@ export default function RootLayout({
           content="SD Designs is an Interior Design firm that operates in Delhi, Faridabad, Noida and Gurgaon / Gurugram (NCR). We have the most skilled expert interior designers in Delhi NCR."
         />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <title>SD Designs: Interior Designers in Delhi NCR</title>
+        <title>SD Designs: Best Interior Designers in Delhi NCR</title>
+        <meta
+          property="og:title"
+          content="SD Designs: Best Interior Designers in Delhi NCR"
+        />
+        <meta
+          name="twitter:title"
+          content="SD Designs: Best Interior Designers in Delhi NCR"
+        />
       </head>
       <body
         className={clsx(
