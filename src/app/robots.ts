d4d9b9a9edf7820765c3ea137d2c6edd8982279e
salhotra <nishantsalhotra.ns@gmail.com>
2024-05-15 17:53:20 +0530

    Add robot txt generation

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://sd-designs.in/sitemap.xml",
+  };
+}
