905587dc1d71d4299b48119d155adec36f72cee9
salhotra <nishantsalhotra.ns@gmail.com>
2024-05-23 01:58:09 +0530

    Add montserrat ultra bold font

--- src/app/fonts/index.ts
+++ src/app/fonts/index.ts
@@ -2,3 +2,7 @@ import { Playfair_Display, Montserrat } from "next/font/google";
 
 export const playfair = Playfair_Display({ subsets: ["latin"] });
 export const montserrat = Montserrat({ subsets: ["latin"], weight: "200" });
+export const montserratUltraBold = Montserrat({
+  subsets: ["latin"],
+  weight: "900",
+});
